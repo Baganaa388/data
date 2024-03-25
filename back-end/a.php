81a4a4ddca16b51d8401f2a608c843d98d5be81c
Baganaa388 <145312219+Baganaa388@users.noreply.github.com>
2024-03-25 23:22:04 +0800

    Delete back-end directory

--- back-end/a.php
+++
@@ -1,8 +0,0 @@
-<?php     
-include("conn.php");
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
-  echo "Connected successfully";
-?>
-
--- back-end/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost:3306";
-$username = "root";
-$password = "Sict@057";
-$database = "db";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-?>
